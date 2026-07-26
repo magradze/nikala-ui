e9e3144d86371949589030674c20eb9df099911c
Giorgi Magradze <magradze.dev@gmail.com>
2026-07-26 19:15:50 +0200

    refactor(docs): rename SiteHeader to Header in DocsLayout component

--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,13 +1,13 @@
 import type { RouteSectionProps } from "@solidjs/router";
 import type { Component } from "solid-js";
-import { SiteHeader } from "~/components/partials/header";
+import { Header } from "~/components/partials/header";
 import { DocsSidebar } from "@/components/docs-sidebar";
 
 const DocsLayout: Component<RouteSectionProps> = (props) => {
   return (
     <div class="relative min-h-screen flex flex-col bg-background text-foreground">
       {/* Global Application Header */}
-      <SiteHeader />
+      <Header />
 
       <div class="container max-w-screen-2xl flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)] gap-6 px-4 md:px-8">
         {/* Left Documentation Sidebar Navigation */}
